f9e529d5cb0cbf1afb0441b3b753e71578687ebc
Amanda Murphy <helloamandamurphy@gmail.com>
2018-07-23 15:08:29 +0000

    automatically backed up by learn

--- test_option.rb
+++ test_option.rb
@@ -1,6 +1,6 @@
 #Empty Board
 def display_board(board = " ")
-board[4] = "X"
+board[4] = X
 puts " #{board[0]} | #{board[1]} | #{board[2]} "
 puts "-----------"
 puts " #{board[3]} | #{board[4]} | #{board[5]} "
